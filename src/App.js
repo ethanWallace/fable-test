e52bc1e56b65ee5b2609141bbd17ac73cf092ea3
Ethan Wallace <ethan.wallace.91@gmail.com>
2023-07-12 15:18:51 -0400

    gh-pages package for deploying

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { HashRouter as Router, Route, Routes} from "react-router-dom";
 import MultiTest from "./Components/MultiTest";
 import LargeForm from "./Components/LargeForm";
 import Page3 from "./Components/Page3";
