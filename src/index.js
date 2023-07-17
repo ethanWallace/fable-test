30e838752e564499111712c4544c19e922d11e6b
Ethan Wallace <ethan.wallace.91@gmail.com>
2023-07-17 15:19:37 -0400

    Remove unused HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import App from './App';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
